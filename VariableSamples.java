f4e5a5971f9407699aed80f5958b78d3269758e4
jc-yason <jc.yason@outlook.com>
2019-01-27 15:34:56 +0800

    Variable Sample

---
+++ VariableSamples.java
@@ -0,0 +1,27 @@
+public class VariableSamples {
+	public static void main( String[] args ) {
+
+	boolean result = true;
+	char option;
+	option = 'C';
+	double grade = 0.0;
+
+	int counter = 5;
+
+	System.out.println("Result: " + result);
+	System.out.println("Option: " + option);
+	System.out.println("Grade: " + grade);
+	System.out.println("Counter: " + counter);
+
+	result = false;
+	option = 'G';
+	grade = 1.0;
+
+	counter++;
+
+	System.out.println("Result: " + result);
+	System.out.println("Option: " + option);
+	System.out.println("Grade: " + grade);
+	System.out.println("Counter: " + counter);
+	}
+}
